9a18096191178451037e0321d32d986517b6068c
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-10-19 13:56:13 +0200

    Basic spring setup with spring assistant

---
+++ src/main/java/com/example/pokeapi/SpringWithPokeapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithPokeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithPokeapiApplication.class, args);
+	}
+
+}
